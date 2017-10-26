6b4a74208a1f9acff59211c32050927604f8526f
William S. Woodruff <williamswoodruff@gmail.com>
2017-10-26 13:14:52 -0400

    Remove password from mysql connection

--- test/knexfile.js
+++ test/knexfile.js
@@ -15,7 +15,6 @@ module.exports = [
         connection: {
             host: '127.0.0.1',
             user: 'root',
-            password: 'password',
             database: 'schwifty_migration_test'
         },
         pool: {
