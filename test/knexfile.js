3db069b6fc4feb19b303f3590cdaf1450d5f6fe9
William S. Woodruff <williamswoodruff@gmail.com>
2017-10-26 13:11:10 -0400

    Fix tests for mysql

--- test/knexfile.js
+++ test/knexfile.js
@@ -15,6 +15,7 @@ module.exports = [
         connection: {
             host: '127.0.0.1',
             user: 'root',
+            password: 'password',
             database: 'schwifty_migration_test'
         },
         pool: {
